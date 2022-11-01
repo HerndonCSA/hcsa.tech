990f0448b57bca9ab01628d1470c5b5e577bc881
soos <62181284+sooswastaken@users.noreply.github.com>
2022-11-01 17:20:01 -0400

    Added more css

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 new FlipDown(1669368000, {
-    theme: "light",
+    theme: "dark",
   }).start();
+
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
