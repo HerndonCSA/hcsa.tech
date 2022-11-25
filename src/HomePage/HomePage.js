67c3bf2f7f49e598a2d0b3b2d7deb1753307543e
soos <62181284+sooswastaken@users.noreply.github.com>
2022-11-25 12:19:23 -0500

    Layout & CSS Rewrite

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,35 +1,45 @@
 export default function HomePage() {
   return (
     <>
+    <div className="main">
       <div className="login">
-        <a href="https://google.com">Login</a>
+        <h1>Login</h1>
       </div>
-      <div className="main">
-        <div className="welcome">
-          <h1>We are the <strong>Herndon</strong></h1>
-          <div className="name">
-            <h3>Computer Science Association</h3>
-            <div className="shortcut">Just call us the "HCSA"</div>
-          </div>
-          <h3 className="info">A place for students to explore the countless fields of computer science.</h3>
+      <div className="nav">
+        <ul className="items">
+          <li className="home">
+            <a href="/">Home</a>
+          </li>
+          <li className="members">
+            <a href="/members">View Our Members</a>
+          </li>
+          <li className="contact">
+            <a href="/contact">Contact Us</a>
+          </li>
+        </ul>
+        <div className="selector"/>
+      </div>
+      <div className="welcome">
+        <h1>We are the <span>Herndon</span></h1>
+        <div className="name">
+          <h1>Computer Science Association</h1>
+          <div className="shortcut">Just call us the "HCSA"</div>
         </div>
+        <h3 className="info">A place for students to explore the countless fields of computer science.</h3>
       </div>
-      <div className="secondary">
-        <div className="buttons">
-          <div className="view">View what we're working on</div>
+      <div className="content">
+        <div className="join_and_view">
+          <img src="/" alt="waves" />
+          <button className="view">View what we're working on</button>
           <div className="or">
-            <div className="line"></div>
-            <p>or</p>
-            <div className="line"></div>
+            <div className="line"/>
+            <div className="text">or</div>
+            <div className="line"/>
           </div>
-          <div className="join">Join us</div>
+          <button className="join">Join us</button>
         </div>
-      <div class="ocean">
-        <div class="wave"></div>
-        <div class="wave"></div>
-        <div class="wave"></div>
       </div>
-      </div>
+    </div>
     </>
   );
 }
