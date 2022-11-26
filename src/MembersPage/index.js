f94f6948f0c7239a18654865ded9ba584061f374
darklordrepo <76636689+darklordrepo@users.noreply.github.com>
2022-11-26 20:56:50 +0000

    card

--- src/MembersPage/index.js
+++ src/MembersPage/index.js
@@ -1,18 +1,52 @@
 import { motion } from "framer-motion";
 
-export default function MembersPage({enterAnimation, exitAnimation, animationVariants}) {
-    return (
-      <motion.div className="container"
-        key="members"
-        variants={animationVariants}
-        initial={animationVariants[enterAnimation]}
-        animate="visible"
-        exit={animationVariants[exitAnimation]}
-        transition={{ duration: 0.25 }}
-      >
-            <div className="members">
-                <h1>Members</h1>
+export default function MembersPage({ enterAnimation, exitAnimation, animationVariants }) {
+  return (
+    <motion.div className="container"
+      key="members"
+      variants={animationVariants}
+      initial={animationVariants[enterAnimation]}
+      animate="visible"
+      exit={animationVariants[exitAnimation]}
+      transition={{ duration: 0.25 }}
+    >
+      <div className="members">
+        <h1>Members</h1>
+      </div>
+
+      <div className="user-card">
+        <div className="card-content">
+
+          {/* user name area */}
+          <div className="user-name">
+            <div className="username-F">
+              Sudarshan
             </div>
-        </motion.div>
-    );
+            <div className="username-L">
+              Lamichhane
+            </div>
+          </div>
+
+          {/* connections area */}
+          <div className="connections">
+            <div className="connection">
+              <div className="connection-icon">
+                <img src="https://www.svgrepo.com/show/331368/discord-v2.svg" width="25" height="25" alt="Discord logo"></img>
+              </div>
+              <div className="connection-id">
+                soos#6008
+              </div>
+            </div>
+          </div>
+
+          {/* email area */}
+          <div className="email">
+            soos@hcsa.tech
+          </div>
+
+        </div>
+      </div>
+
+    </motion.div>
+  );
 }
