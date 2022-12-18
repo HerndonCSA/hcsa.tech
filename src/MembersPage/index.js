2a5acc3433fb353b6d8ced5eebc6081ebfef1027
darklordrepo <bldragonh1@gmail.com>
2022-12-17 23:31:27 -0500

    added data.js to store card data

--- src/MembersPage/index.js
+++ src/MembersPage/index.js
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import "./assets/style.css";
+import users from "./data.js";
 
 export default function MembersPage({ enterAnimation, exitAnimation, animationVariants }) {
   return (
@@ -9,212 +11,42 @@ export default function MembersPage({ enterAnimation, exitAnimation, animationVa
       exit={{ opacity: 0 }}
       transition={{ duration: 0.25 }}
     >
-      <div class="welcome"><h3>Our Members</h3></div>
-
-      <div className="user-card">
-        <div className="card-content">
-
-          {/* user name area */}
-          <div className="user-name">
-            <div className="username-F">
-            Sudarshan
-            </div>
-            <div className="username-L">
-              Lamichane
-            </div>
-          </div>
-
-          {/* connections area */}
-          <div className="connections">
-            <div className="connection">
-              <div className="connection-icon">
-                <img src="https://www.svgrepo.com/show/331368/discord-v2.svg" width="25" height="25" alt="Discord logo"></img>
-              </div>
-              <div className="connection-id">
-                soos#6008
-              </div>
-            </div>
-          </div>
-
-          {/* email area */}
-          <div className="email">
-            soos@hcsa.tech
-          </div>
-
-        </div>
+      <div class="welcome">
+        <h3>Our Members</h3>
       </div>
 
+      <div className="contains-all-cards">
 
-      <div className="user-card">
-        <div className="card-content">
-          
-          {/* user name area */}
-          <div className="user-name">
-            <div className="username-F">
-              Bairon
-            </div>
-            <div className="username-L">
-              Recinos-Monjaras
-            </div>
-          </div>
-
-          {/* connections area */}
-          <div className="connections">
-            <div className="connection">
-              <div className="connection-icon">
-                <img src="https://www.svgrepo.com/show/331368/discord-v2.svg" width="25" height="25" alt="Discord logo"></img>
+        {/*  map through the users array and return a div for each user */}
+        {users.map((user) => (
+          <div className="user-card">
+            <div className="card-content">
+              <div className="user-name">
+                <div className="username-F">
+                  {user.userNameF}
+                </div>
+                <div className="username-L">
+                  {user.userNameL}
+                </div>
               </div>
-              <div className="connection-id">
-                brn#5538
+              <div className="connections">
+                <div className="connection">
+                  <div className="connection-icon">
+                    <img src="https://www.svgrepo.com/show/331368/discord-v2.svg" width="25" height="25" alt="Discord logo"></img>
+                  </div>
+                  <div className="connection-id">
+                    {user.discord}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-
-          {/* email area */}
-          <div className="email">
-            soos@hcsa.tech
-          </div>
-
-        </div>
-      </div>
-
-      <div className="user-card">
-        <div className="card-content">
-
-          {/* user name area */}
-          <div className="user-name">
-            <div className="username-F">
-            Sudarshan
-            </div>
-            <div className="username-L">
-              Lamichane
-            </div>
-          </div>
-
-          {/* connections area */}
-          <div className="connections">
-            <div className="connection">
-              <div className="connection-icon">
-                <img src="https://www.svgrepo.com/show/331368/discord-v2.svg" width="25" height="25" alt="Discord logo"></img>
-              </div>
-              <div className="connection-id">
-                soos#6008
+              <div className="email">
+                {user.email}
               </div>
             </div>
           </div>
-
-          {/* email area */}
-          <div className="email">
-            soos@hcsa.tech
-          </div>
-
-
-
-        </div>
-      </div>
-
-      <div className="user-card">
-        <div className="card-content">
-
-          {/* user name area */}
-          <div className="user-name">
-            <div className="username-F">
-            Sudarshan
-            </div>
-            <div className="username-L">
-              Lamichane
-            </div>
-          </div>
-
-          {/* connections area */}
-          <div className="connections">
-            <div className="connection">
-              <div className="connection-icon">
-                <img src="https://www.svgrepo.com/show/331368/discord-v2.svg" width="25" height="25" alt="Discord logo"></img>
-              </div>
-              <div className="connection-id">
-                soos#6008
-              </div>
-            </div>
-          </div>
-
-          {/* email area */}
-          <div className="email">
-            soos@hcsa.tech
-          </div>
-
-        </div>
-      </div>
-
-
-      <div className="user-card">
-        <div className="card-content">
-
-          {/* user name area */}
-          <div className="user-name">
-            <div className="username-F">
-              Bairon
-            </div>
-            <div className="username-L">
-              Recinos-Monjaras
-            </div>
-          </div>
-
-          {/* connections area */}
-          <div className="connections">
-            <div className="connection">
-              <div className="connection-icon">
-                <img src="https://www.svgrepo.com/show/331368/discord-v2.svg" width="25" height="25" alt="Discord logo"></img>
-              </div>
-              <div className="connection-id">
-                brn#5538
-              </div>
-            </div>
-          </div>
-
-          {/* email area */}
-          <div className="email">
-            soos@hcsa.tech
-          </div>
-
-        </div>
-      </div>
-
-      <div className="user-card">
-        <div className="card-content">
-
-          {/* user name area */}
-          <div className="user-name">
-            <div className="username-F">
-            Sudarshan
-            </div>
-            <div className="username-L">
-              Lamichane
-            </div>
-          </div>
-
-          {/* connections area */}
-          <div className="connections">
-            <div className="connection">
-              <div className="connection-icon">
-                <img src="https://www.svgrepo.com/show/331368/discord-v2.svg" width="25" height="25" alt="Discord logo"></img>
-              </div>
-              <div className="connection-id">
-                soos#6008
-              </div>
-            </div>
-          </div>
-
-          {/* email area */}
-          <div className="email">
-            soos@hcsa.tech
-          </div>
-
-
-
-        </div>
+        ))
+        }
       </div>
     </motion.div>
-
   );
 }
