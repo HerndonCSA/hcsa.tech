d3b8e114b0e25c19ec42d6bf76fdabb6569f32a6
soos <62181284+sooswastaken@users.noreply.github.com>
2022-12-23 17:00:00 -0500

    Update index.tsx

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -113,7 +113,7 @@ const NavBar = ({ userData, setUserData }: any) => {
           >
             <button
               onClick={() => {
-                window.location.href = "http://localhost:8000/login";
+                window.location.href = "https://api.hcsa.tech/login";
               }}
             >
               Sign in
