a35b82ea36d27bef7c9a72e16551eb519b176841
DarkLord-GitHub <76636689+DarkLord-GitHub@users.noreply.github.com>
2022-12-23 21:05:02 +0000

    set width of udermenu to the same width as profile component

--- src/components/NavBar/components/Profile.tsx
+++ src/components/NavBar/components/Profile.tsx
@@ -1,18 +1,31 @@
 import ArrowIcon from "../assets/images/arrow-down.svg";
 
-const Profile = ({ userData, dropdownOpen, setDropdownOpen, profileRef }: { userData: any; dropdownOpen: boolean; setDropdownOpen: any; profileRef: any }) => {
-    return (
-        <div className="profile"
-            onClick={() => {
-                setDropdownOpen(!dropdownOpen);
-            }}
-            ref={profileRef}
-        >
-            <img src={userData.picture} alt="profile" referrerPolicy="no-referrer" />
-            <p>{userData.firstName}</p>
-            <img src={ArrowIcon} alt="arrow icon" />
-        </div >
-    );
-}
+const Profile = ({
+  userData,
+  dropdownOpen,
+  setDropdownOpen,
+  profileRef,
+  dropdownRef,
+}: {
+  userData: any;
+  dropdownOpen: boolean;
+  setDropdownOpen: any;
+  profileRef: any;
+  dropdownRef: any;
+}) => {
+  return (
+    <div
+      className="profile"
+      onClick={() => {
+        setDropdownOpen(!dropdownOpen);
+      }}
+      ref={profileRef}
+    >
+      <img src={userData.picture} alt="profile" referrerPolicy="no-referrer" />
+      <p>{userData.firstName}</p>
+      <img src={ArrowIcon} alt="arrow icon" />
+    </div>
+  );
+};
 
 export default Profile;
