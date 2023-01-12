150347f306a6cd132ded8ab393ae2f2cf7dc6f91
soos <62181284+sooswastaken@users.noreply.github.com>
2023-01-12 14:02:28 -0500

    Update data.ts

--- src/pages/MembersPage/data.ts
+++ src/pages/MembersPage/data.ts
@@ -1,40 +1,39 @@
 const users = [
     {
-        userNameF: "Bruv",
-        userNameL: "Bdfuh",
-        discord: "jisdfij#6008",
-        github: "@asdjlnnf",
-        email: "asdf@hcsa.tech"
+        userNameF: "Sudarshan",
+        userNameL: "Lamichhane",
+        discord: "soos#6008",
+        github: "@sooswastaken",
+        email: "soos@hcsa.tech"
     },
     {
-        userNameF: "AF",
-        userNameL: "SDFsdf",
-        discord: "DSfsdf#5380",
-        github: "@sdfsdf-GitHub",
-        email: "sdfsdf.sdf@hcsa.tech"
+        userNameF: "Sachin",
+        userNameL: "Khanal",
+        discord: "DarkLord#5380",
+        github: "@darklord-GitHub",
+        email: "sachin.khanal@hcsa.tech"
     },
     {
-        userNameF: "DFsdfds",
-        userNameL: "DFSDf-dsDF#",
-        discord: "FSDF#5538",
-        github: "@Wafsdfsdf",
-        email: "2sfsdf.sdfsdfs@hcsa.tech"
+        userNameF: "Bairon",
+        userNameL: "Recinos-Monjaras",
+        discord: "brn#5538",
+        github: "@baironrecinos",
+        email: "bairon.recinos@hcsa.tech"
     },
     {
-        userNameF: "@#fsdfsdf3",
-        userNameL: "DFF-SDFsdf3",
-        discord: "ADFSSdfs#7795",
-        github: "@SDFef3e",
-        email: "SDFe@SDFsdf.tech"
+        userNameF: "Johnathan",
+        userNameL: "Ayala-Dubon",
+        discord: "JonathanA12345#7795",
+        github: "@Johnathan2022",
+        email: "Jonathan@hcsa.tech"
     },
     {
-        userNameF: "SDF3wfsd",
-        userNameL: "SDFe-dsf3e",
-        discord: "DSF3#7096",
-        github: "@dsf3",
-        email: "sdf3.FSdf3@hcsa.tech"
+        userNameF: "Evan",
+        userNameL: "Sanchez-Alvarez",
+        discord: "Retroneek#7096",
+        github: "@Retroneek",
+        email: "evan.sanchez@hcsa.tech"
     }
 ]
-
 
 export default users;
