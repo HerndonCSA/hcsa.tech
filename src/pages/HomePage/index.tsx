ac13b3bbdae7f43548425cde8b406eae2ec8262d
soos <62181284+sooswastaken@users.noreply.github.com>
2023-02-10 10:25:57 -0500

    Fixed typo (also added grammarly)

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,41 +3,41 @@ import { motion } from "framer-motion";
 import { useEffect } from "react";
 
 const HomePage = () => {
-  return (
-    <motion.div
-      className="homepage"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.25 }}
-    >
-      <div className="welcome-message">
-        <h1 className="wm-1">
-          We are the
-          <span> Herndon</span>
-        </h1>
-        <h2 className="wm-2">Computer Science Assocation - HCSA</h2>
-        <h3 className="wm-3">
-          A place for students to explore the countless fields of computer
-          science.
-        </h3>
-      </div>
+	return (
+		<motion.div
+			className="homepage"
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 0.25 }}
+		>
+			<div className="welcome-message">
+				<h1 className="wm-1">
+					We are the
+					<span> Herndon</span>
+				</h1>
+				<h2 className="wm-2">Computer Science Association - HCSA</h2>
+				<h3 className="wm-3">
+					A place for students to explore the countless fields of
+					computer science.
+				</h3>
+			</div>
 
-      <div className="interactive-content">
-        <div className="view-or-join">
-          <button className="view">View Our Ongoing Projects</button>
+			<div className="interactive-content">
+				<div className="view-or-join">
+					<button className="view">View Our Ongoing Projects</button>
 
-          <div className="or">
-            <div className="line"></div>
-            <p>or</p>
-            <div className="line"></div>
-          </div>
+					<div className="or">
+						<div className="line"></div>
+						<p>or</p>
+						<div className="line"></div>
+					</div>
 
-          <button className="join">Join Us</button>
-        </div>
-      </div>
-    </motion.div>
-  );
+					<button className="join">Join Us</button>
+				</div>
+			</div>
+		</motion.div>
+	);
 };
 
 export default HomePage;
